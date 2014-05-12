66edf53f91d3bdf600135a5c21bc804f1d6ce3d0
Robert Williams <rob@r-williams.com>
2014-05-12 15:54:49 +0100

    Parse request.

---
+++ notebook.rb
@@ -0,0 +1,24 @@
+require 'socket'
+
+server = TCPServer.new(9292)
+
+loop do
+  socket = server.accept
+
+  lines = []
+
+  loop do
+    line = socket.readline.strip
+    break if line.empty?
+    lines << line
+
+  end
+
+  request_line, *header_lines = lines
+
+  request_method, request_path, http_version = request_line.split(" ")
+
+  puts "request_method: #{request_method}, request_path: #{request_path}, "\
+       "http_version: #{http_version}"
+end
+
