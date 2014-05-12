4f0e7f979c43b3403ff691f66c23ad15e1343ecd
Robert Williams <rob@r-williams.com>
2014-05-12 16:15:37 +0100

    Load content from a database.

--- notebook.rb
+++ notebook.rb
@@ -1,8 +1,10 @@
 require 'socket'
+require 'sqlite3'
 
 CRLF = "\r\n"
 
 server = TCPServer.new(9292)
+database = SQLite3::Database.new('notebook.sqlite3')
 
 loop do
   socket = server.accept
@@ -20,11 +22,23 @@ loop do
 
   request_method, request_path, http_version = request_line.split(" ")
 
+  response_body = ''
 
+  case [request_method, request_path]
+  when ['GET', '/show-notes']
+    # show all the notes
+    response_body << "<ul>"
 
-  response_body = "request_method: #{request_method},"\
-                  "request_path: #{request_path}, "\
-                  "http_version: #{http_version}"
+    database.execute('SELECT content FROM notes') do |(content)|
+      response_body << "<li>#{content}</li>"
+    end
+
+    response_body << "</ul>"
+  else
+    response_body = "request_method: #{request_method},"\
+                    "request_path: #{request_path}, "\
+                    "http_version: #{http_version}"
+  end
 
   socket.write 'HTTP/1.1 200 OK' + CRLF
   socket.write 'Content-Type: text/html' + CRLF
@@ -32,4 +46,3 @@ loop do
   socket.write response_body
   socket.close
 end
-
