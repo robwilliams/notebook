2b48d20119019c325a0fa0c191a81ff010eb7ffe
Robert Williams <rob@r-williams.com>
2014-05-12 16:53:04 +0100

    Move to Rack

--- notebook.rb
+++ notebook.rb
@@ -1,69 +1,39 @@
-require 'cgi'
-require 'socket'
 require 'sqlite3'
 require 'uri'
 
-CRLF = "\r\n"
-
-server = TCPServer.new(9292)
 database = SQLite3::Database.new('notebook.sqlite3')
 
-loop do
-  socket = server.accept
+Notebook = -> env do
+  request = Rack::Request.new(env)
+  request_method, request_path = env['REQUEST_METHOD'], env['REQUEST_PATH']
 
-  lines = []
+  Rack::Response.new do |response|
 
-  loop do
-    line = socket.readline.strip
-    break if line.empty?
-    lines << line
+    if request.get? && request.path == '/show-notes'
+      response['Content-Type'] = 'text/html'
+      response.write "<ul>"
 
-  end
+      database.execute('SELECT content FROM notes') do |(content)|
+        response.write "<li>#{CGI.escapeHTML(content)}</li>"
+      end
 
-  request_line, *header_lines = lines
+      response.write "</ul>"
 
-  request_method, request_path, http_version = request_line.split(" ")
-
-  response_body = ''
-
-  case [request_method, request_path]
-  when ['GET', '/show-notes']
-    # show all the notes
-    response_body << "<ul>"
-
-    database.execute('SELECT content FROM notes') do |(content)|
-      response_body << "<li>#{CGI.escapeHTML(content)}</li>"
-    end
-
-    response_body << "</ul>"
-
-    response_body << %q{
+      response.write %q{
       <form action="/create-note" method="post">
 
         <input name="content" maxlength="140">
         <input type="submit">
       </form>
-    }
-  when ['POST', '/create-note']
-    content_length_header = header_lines.detect{ |line|
-      line.start_with?("Content-Length")
-    }
-    content_length = content_length_header.slice(/\d+/).to_i
-    request_body = socket.read(content_length)
-    params = Hash[URI.decode_www_form(request_body)]
-    content = params['content']
-    database.execute('INSERT INTO notes VALUES (?)', content)
+      }
+    elsif request.post? && request.path == "/create-note"
+      content = request.params['content']
+      database.execute('INSERT INTO notes VALUES (?)', content)
 
-    response_body << "Posted: #{CGI.escape_html(content)}, <a href='/show-notes'>go back</a>"
-  else
-    response_body = "request_method: #{request_method},"\
-                    "request_path: #{request_path}, "\
-                    "http_version: #{http_version}"
+      response.redirect('/show-notes', 303) # See other
+    else
+      response.write "request_method: #{request_method},"\
+        "request_path: #{request.path}"
+    end
   end
-
-  socket.write 'HTTP/1.1 200 OK' + CRLF
-  socket.write 'Content-Type: text/html' + CRLF
-  socket.write CRLF
-  socket.write response_body
-  socket.close
 end
